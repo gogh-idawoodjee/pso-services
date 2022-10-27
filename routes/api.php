382fef908004b78a1e05df09d10afd81cf9ff198
Ish <idawoodjee@mac.com>
2022-10-27 11:50:16 -0400

    updated api routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\PSOActivityController;
+use App\Http\Controllers\PSOActivitySLAController;
+use App\Http\Controllers\PSOActivityStatusController;
 use App\Http\Controllers\PSOCommitController;
 use App\Http\Controllers\PSOResourceController;
 use App\Http\Controllers\PSOResourceEventController;
 use App\Http\Controllers\PSOResourceShiftController;
+use App\Http\Controllers\PSOUnavailabilityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,8 +27,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::patch('/commit', [PSOCommitController::class, 'update']);
+
+
+Route::delete('/activity/{activity_id}/sla', [PSOActivitySLAController::class, 'destroy']);
+
+
+Route::patch('/activity/{activity_id}/{status}', [PSOActivityStatusController::class, 'update']);
+
+
 Route::patch('/resource/{resource_id}/manualschedule', [PSOResourceShiftController::class, 'update']);
 Route::post('/resource/{resource_id}/event', [PSOResourceEventController::class, 'store']);
+Route::post('/resource/{resource_id}/unavailability', [PSOUnavailabilityController::class, 'store']);
+Route::post('/resource/{resource_id}/unavailability/{unavailability_id}', [PSOUnavailabilityController::class, 'update']);
+Route::post('/resource/{resource_id}/unavailability/{unavailability_id}/delete', [PSOUnavailabilityController::class, 'destroy']);
 Route::get('/resource/', [PSOResourceController::class, 'index']);
 Route::get('/resource/{resource_id}', [PSOResourceController::class, 'show']);
 
