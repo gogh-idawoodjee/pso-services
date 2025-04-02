c4589cabd8907da02f83777596c6174580273c73
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-04-02 17:45:36 -0400

    removed old views

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,18 @@ Route::get('/', static function () {
 ////    return view('dashboard');
 //})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    // I think all this has moved to the front-end project
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//
-//    Route::get('/environment', [EnvironmentController::class, 'index'])->name('env.index');
-//    Route::get('/environment/{id}', [EnvironmentController::class, 'edit'])->name('env.edit');
-//    Route::get('/assist/init', [AssistController::class, 'index'])->name('assist.index');
-//    Route::get('/assist/rota', [AssistController::class, 'update'])->name('assist.update');
-//    Route::get('/assist/usage', [AssistController::class, 'show'])->name('assist.show');
-//    Route::get('/resource', [ResourceController::class, 'index'])->name('resource.show');
-});
+//Route::middleware('auth')->group(function () {
+//    // I think all this has moved to the front-end project
+////    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+////    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+////    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+////
+////    Route::get('/environment', [EnvironmentController::class, 'index'])->name('env.index');
+////    Route::get('/environment/{id}', [EnvironmentController::class, 'edit'])->name('env.edit');
+////    Route::get('/assist/init', [AssistController::class, 'index'])->name('assist.index');
+////    Route::get('/assist/rota', [AssistController::class, 'update'])->name('assist.update');
+////    Route::get('/assist/usage', [AssistController::class, 'show'])->name('assist.show');
+////    Route::get('/resource', [ResourceController::class, 'index'])->name('resource.show');
+//});
 
-require __DIR__ . '/auth.php';
+//require __DIR__ . '/auth.php';
