63d2af6bca15f5ebec3532dbe63620e3daa2c254
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-04-26 22:22:42 -0400

    refactored base service

--- app/Classes/BaseService.php
+++ app/Classes/BaseService.php
@@ -2,7 +2,27 @@
 
 namespace App\Classes;
 
-class BaseService
+
+use App\Traits\V2\PSOAssistV2;
+use SensitiveParameter;
+
+abstract class BaseService
 {
+
+    use PSOAssistV2;
+
+    protected string|null $sessionToken;
+
+
+    public function __construct(#[SensitiveParameter] string|null $sessionToken = null)
+    {
+        $this->sessionToken = $sessionToken;
+    }
+
+    public function sendPayloadToPso(array $payload)
+    {
+
+
+    }
 
 }
