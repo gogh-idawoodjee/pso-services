23738b57bbc7e84756a606ae8c7acf02e26c0129
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-04-26 22:22:58 -0400

    added status route

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\V2\ActivityStatusController;
 use App\Http\Controllers\Api\V2\HealthCheckController;
 use App\Http\Controllers\Api\V2\TravelController;
 
 Route::post('/health-check', [HealthCheckController::class, 'check']);
+
+// travel routes
 Route::post('/travelanalyzer', [TravelController::class, 'store']); // this is the main service -> send data, returns an ID of some sort
 Route::post('/travelanalyzerservice', [TravelController::class, 'update']); // this is the broadcast listener
 Route::get('/travelanalyzer/{id}', [TravelController::class, 'show']); // this is the return service that returns your details once ready
+
+
+//Activity routes
+Route::patch('/activity/{activityId}/status', [ActivityStatusController::class, 'update']); // added /status in case someday there is an update to the actual activity object itself
