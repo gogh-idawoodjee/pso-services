a8c7649f663f8fe39eb9079f90b5e3292796f3ec
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-05-09 11:47:52 -0400

    Update 2023_07_12_181939_create_psorun_table.php

--- database/migrations/2023_07_12_181939_create_psorun_table.php
+++ database/migrations/2023_07_12_181939_create_psorun_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('psorun', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('sla_type_id');
             $table->string('dataset_id');
             $table->string('username');
