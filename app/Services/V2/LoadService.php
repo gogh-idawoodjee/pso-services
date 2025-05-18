d26f2607f4e2f248d738ab2962002f024f283a0f
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-05-18 11:31:02 -0400

    refactored loadPSO

--- app/Services/V2/LoadService.php
+++ app/Services/V2/LoadService.php
@@ -4,6 +4,7 @@ namespace App\Services\V2;
 
 use App\Classes\V2\BaseService;
 use App\Classes\V2\EntityBuilders\InputReferenceBuilder;
+use App\Constants\PSOConstants;
 use App\Enums\InputMode;
 use App\Enums\ProcessType;
 use App\Helpers\PSOHelper;
@@ -11,7 +12,6 @@ use App\Helpers\Stubs\SourceData;
 use App\Helpers\Stubs\SourceDataParameter;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\JsonResponse;
-use JsonException;
 use SensitiveParameter;
 
 class LoadService extends BaseService
@@ -31,61 +31,71 @@ class LoadService extends BaseService
      */
     public function loadPSO(): JsonResponse
     {
+        // Extracting environment block
+        $environment = data_get($this->data, 'environment');
+        $datasetId = data_get($environment, 'datasetId');
+        $baseUrl = data_get($environment, 'baseUrl');
 
-        $datasetId = data_get($this->data, 'environment.datasetId');
-        $datetime = data_get($this->data, 'data.datetime');
-        $dseDuration = PSOHelper::setPSODurationDays(data_get($this->data, 'data.dseDuration'));
-        $processType = Processtype::from(data_get($this->data, 'data.processType'));
-        $appointmentWindow = data_get($this->data, 'data.appointmentWindow') ? PSOHelper::setPSODurationDays(data_get($this->data, 'data.appointmentWindow')) : null;
-        $id = data_get($this->data, 'data.Id');
-        $description = data_get($this->data, 'data.description');
+        // Extracting data block
+        $data = data_get($this->data, 'data');
+        $datetime = data_get($data, 'datetime');
+        $id = data_get($data, 'Id');
+        $description = data_get($data, 'description');
+        $dseDuration = PSOHelper::setPSODurationDays(data_get($data, 'dseDuration'));
+        $processType = Processtype::from(data_get($data, 'processType'));
+        $appointmentWindowRaw = data_get($data, 'appointmentWindow');
+        $appointmentWindow = $appointmentWindowRaw ? PSOHelper::setPSODurationDays($appointmentWindowRaw) : null;
+        $includeArpData = data_get($data, 'includeArpData', false);
+        $keepPsoData = data_get($data, 'keepPsoData', false);
+        $sendToPso = data_get($data, 'sendToPso', false);
 
-        $payload =
-            InputReferenceBuilder::make($datasetId)
-                ->inputType(InputMode::LOAD)
-                ->dateTime($datetime)
-                ->dseDuration($dseDuration)
-                ->processType($processType)
-                ->appointmentWindow($appointmentWindow)
-                ->id($id)
-                ->description($description)
-                ->build();
-        $keepPsoData = data_get($this->data, 'data.keepPsoData');
-        $sendToPso = data_get($this->data, 'data.sendToPso');
+        // Build Input Reference
+        $inputRef = InputReferenceBuilder::make($datasetId)
+            ->inputType(InputMode::LOAD)
+            ->dateTime($datetime)
+            ->dseDuration($dseDuration)
+            ->processType($processType)
+            ->appointmentWindow($appointmentWindow)
+            ->id($id)
+            ->description($description)
+            ->build();
+
+        // Start payload
+        $payload = ['Input_Reference' => $inputRef];
+
+        // Add optional ARP source data
+        if ($includeArpData) {
+            $payload['Source_Data'] = SourceData::make();
+            $payload['Source_Data_Parameter'] = SourceDataParameter::make(
+                PSOConstants::SOURCE_DATA_PARAM_NAME,
+                PSOConstants::SOURCE_DATA_PARAM_VALUE
+            );
+        }
+
+        // Handle keep/send flags
         $keepPsoDataMessage = null;
 
-        if (data_get($this->data, 'data.includeArpData')) {
-            // todo finish this part
-            $sourceData = SourceData::make();
-            $sourceDataParam = SourceDataParameter::make('rota_id', 'master');
-        }
 
-        $baseUrl = data_get($this->data, 'environment.baseUrl');
-
-        if ($keepPsoData && $sendToPso) {
-            $keepPsoDataMessage = 'Keeping Existing PSO Data';
-
-            // todo test this - we have to assume we have the token because sendToPSO is true
-            $scheduleData = ScheduleService::getScheduleData($baseUrl, $datasetId, $this->sessionToken);
-            $payload = collect($payload)->merge($scheduleData)->toArray();
-
-        }
-        if ($keepPsoData && !$sendToPso) {
-            $keepPsoDataMessage = 'Attention: Request to Keep PSO Data but not sending to PSO.';
+        if ($keepPsoData) {
+            if ($sendToPso) {
+                $keepPsoDataMessage = 'Keeping Existing PSO Data';
+                $scheduleData = ScheduleService::getScheduleData($baseUrl, $datasetId, $this->sessionToken);
+                $payload = array_merge($payload, $scheduleData);
+            } else {
+                $keepPsoDataMessage = 'Attention: Request to Keep PSO Data but not sending to PSO.';
+            }
         }
 
         return $this->sendOrSimulateBuilder()
             ->payload($payload)
-            ->environment(data_get($this->data, 'environment'))
+            ->environment($environment)
             ->token($this->sessionToken)
-            ->notSentKey('Input Reference')
             ->additionalDetails($keepPsoDataMessage)
             ->send();
-
     }
 
+
     /**
-     * @throws JsonException
      */
     public function updateRota(): JsonResponse
     {
@@ -95,15 +105,21 @@ class LoadService extends BaseService
         $id = data_get($this->data, 'data.Id');
         $description = data_get($this->data, 'data.description');
 
-        $payload =
-            InputReferenceBuilder::make($datasetId)
+        $payload = [
+            'Input_Reference' => InputReferenceBuilder::make($datasetId)
                 ->inputType(InputMode::CHANGE)
                 ->dateTime($datetime)
                 ->id($id)
                 ->description($description)
-                ->build();
+                ->build(),
 
-        // todo this still needs source data
+            'Source_Data' => SourceData::make(),
+
+            'Source_Data_Parameter' => SourceDataParameter::make(
+                PSOConstants::SOURCE_DATA_PARAM_NAME,
+                PSOConstants::SOURCE_DATA_PARAM_VALUE,
+            ),
+        ];
 
 
         return $this->sendOrSimulateBuilder()
