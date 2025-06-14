11d31c890c914cdfda71896c619cc3960fda7d32
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-06-14 10:49:20 -0400

    add labels

--- app/Enums/AppointmentRequestStatus.php
+++ app/Enums/AppointmentRequestStatus.php
@@ -11,4 +11,20 @@ enum AppointmentRequestStatus: int
     case CHECKED = 3;
     case FAILED = -1;
 
+    public function label(): string
+    {
+        return match ($this) {
+            self::UNACKNOWLEDGED => 'UNACKNOWLEDGED',
+            self::ACCEPTED => 'ACCEPTED',
+            self::DECLINED => 'DECLINED',
+            self::CHECKED => 'CHECKED',
+            self::FAILED => 'FAILED',
+        };
+    }
+
+    public function isAcceptedOrDeclined(): bool
+    {
+        return $this === self::ACCEPTED || $this === self::DECLINED;
+    }
+
 }
